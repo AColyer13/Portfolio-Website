30d6082579c42e90f0a7895ef704db83f74d1312
AColyer13 <adamcolyer@gmail.com>
2025-12-03 20:36:09 -0600

    vite update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   root: 'docs', // Use docs/ as the project root
+  base: '/Portfolio-Website/', // Set base for GitHub Pages
   plugins: [react()],
   build: {
     outDir: '../docs/dist', // Output build to docs/dist
